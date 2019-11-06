d7ec644998e294c53ef373bed7be0083c3a942a2
Kiefer <kiefer.yap@gmail.com>
2019-11-06 20:28:31 +0900

    Removed database configuration

--- paw/settings.py
+++ paw/settings.py
@@ -97,17 +97,6 @@ WSGI_APPLICATION = 'paw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -118,15 +107,6 @@ DATABASES = {
 #         'PORT': config('DB_PORT'),
 #     }
 # }
-
-# Test database for development
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
