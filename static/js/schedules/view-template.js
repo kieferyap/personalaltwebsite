c9fea8605b8e8b2908d0933846a99d3cfc0dd163
Kiefer <kiefer.yap@gmail.com>
2020-07-27 09:52:55 +0900

    Minor changes

--- static/js/schedules/view-template.js
+++ static/js/schedules/view-template.js
@@ -1,11 +1,5 @@
 var WEEKLY_SCHEDULE = WEEKLY_SCHEDULE || {};
 
-WEEKLY_SCHEDULE.clickAddButton = function(e) {
-	var $this = $(e.target);
-	$('#add-class-school-period-id').val($this.data('school-period-id'));
-	$('#weekday-number').val($this.data('weekday'));
-};
-
 WEEKLY_SCHEDULE.clickEditButton = function(e) {
 	var $this = $(e.target);
 	$('#edit-template-section-period-id').val($this.data('template-section-period-id'));
@@ -14,6 +8,5 @@ WEEKLY_SCHEDULE.clickEditButton = function(e) {
 
 $(document).ready(function(){
 	$(document)
-		.on('mousedown', '.add-class-button', WEEKLY_SCHEDULE.clickAddButton)
 		.on('mousedown', '.edit-class-button', WEEKLY_SCHEDULE.clickEditButton);
 });
